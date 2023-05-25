78ccea041444913521cd86a01117e4dda60c97e5
Osiris <pocheshirsky@gmail.com>
2023-05-25 18:18:40 +0400

    Колокольчик

--- src/store/user.js
+++ src/store/user.js
@@ -8,12 +8,15 @@ export default {
         userAnswersArray: [],
         userTestingResult: [],
 
-
         currentRecipient: null,
         newMessages: [],
+        newAlerts: []
     },
+
     getters: {
+        alertsCount: (state)=> state.newAlerts.length
     },
+
     mutations: {
         setUserAnswers(state, userAnswers) {
             state.userAnswersArray = userAnswers
@@ -25,6 +28,7 @@ export default {
             state.newMessages = []
         }
     },
+
     actions: {
         signIn({ state }, userAuthInfo) {
             return api.signIn(userAuthInfo).then((data) => {
@@ -32,27 +36,32 @@ export default {
                 console.log("Авторизация произошла", data.user);
             }).catch((error) => console.log(error))
         },
+
         signUp({ state }, userAuthInfo) {
             return api.signUp(userAuthInfo).then((data) => {
                 state.user = data.user;
                 console.log("Регистрация произошла", data.user);
             }).catch((error) => console.log(error))
         },
+
         getAllUsers() {
             return api.getAllUsers().then((data) => {
                 console.log(data.data);
             }).catch((error) => console.log(error))
         },
+
         updateUserInfo({ state }, userInfo) {
             return api.updateUserInfo(userInfo).then((data) => {
                 state.user = data;
             }).catch((error) => console.log(error))
         },
+
         logout({ state }) {
             return api.logout().then(() => {
                 state.user = null;
             }).catch((error) => console.log(error))
         },
+
         getUserInfo({ state }) {
             return new Promise((resolve, reject) => {
                 if (!state.user && localStorage.getItem('refreshToken')) {
@@ -62,25 +71,28 @@ export default {
                         resolve(data)
                     }).catch((error) => reject(error))
                 } else reject('Пользователь не авторизован или уже существует')
-
             })
         },
+
         createUser(userInfo) {
             return api.createUser(userInfo).then((data) => {
                 console.log(data.data);
             }).catch((error) => console.log(error))
         },
+
         findUserChats(obj, userUuid) {
             return api.findUserChats(userUuid).then((data) => {
                 return data
             }).catch((error) => console.log(error))
         },
+
         findChatMessages(obj, {senderUuid, recipientUuid}) {
             return api.findChatMessages(senderUuid, recipientUuid).then((data) => {
                 console.log(data)
                 return data
             }).catch((error) => console.log(error))
         },
+
         putUserInSearchingPool() {
             return api.putUserInSearchingPool().then((data) => {
                 console.log('Помещаюсь в пул', data);
